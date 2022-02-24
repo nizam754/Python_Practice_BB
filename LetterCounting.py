0daada15ef58b713f174f38e26af2e6c8022be9b
nizam754 <m.nizam754@gmail.com>
2022-02-24 12:43:41 +0600

    Leeter Counting

---
+++ LetterCounting.py
@@ -0,0 +1,13 @@
+# This program counts the frequency of characters on a string.
+
+import pprint as pp
+
+text = 'this is a simple text to TEST the code.'
+
+letters = {}
+
+for i in text:
+    letters.setdefault(i, 0)
+    letters[i] = letters[i]+1
+
+pp.pprint(letters)
