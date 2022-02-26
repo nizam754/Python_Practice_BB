9254878feab3b5d1924fa7191190a4b75c40afb1
nizam754 <m.nizam754@gmail.com>
2022-02-26 20:46:46 +0600

    Decorator part 1

--- Decorator-1.py
+++ Decorator-1.py
@@ -7,5 +7,19 @@ def outer(message):
 
     return inner
 
-f = outer("Hello World")
-f()
+# f = outer("Hello World")
+# f()
+
+def decorator(orginal_func):
+
+    print('In the decorator function\n')
+
+    def wrapper():
+        print(f'wrapper executed before {orginal_func.__name__}()')
+
+        if 10 > 5:
+            print('yes it is true')
+
+        return orginal_func() + ' extra string!'
+
+    return wrapper
